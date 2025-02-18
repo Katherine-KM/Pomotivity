36d4cb94d71859d4bba38e16f155c7c71bdd63d7
Katherine M <katherinesoftwareengineer@gmail.com>
2025-02-18 12:35:01 -0500

    Initial commit: Initial backend setup

---
+++ backend/src/main/java/com/katdev/Pomotivity/PomotivityApplication.java
@@ -0,0 +1,13 @@
+package com.katdev.Pomotivity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PomotivityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PomotivityApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/katdev/Pomotivity/PomotivityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.katdev.Pomotivity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PomotivityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

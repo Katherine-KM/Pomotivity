432d60a041fb9ff5968f3287bc0df69e0217f2a8
Katherine M <katherinesoftwareengineer@gmail.com>
2025-02-21 18:09:20 -0500

    Section B7/B8 - Setting up react and material ui for the front end. Removed initial boiler plate code from react setup.

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import { Container } from '@mui/material'
+
+function App() {
+  return (
+    <>
+    <Container>
+    <div>Hello World!</div>
+    </Container>
+    <div>Hello World!</div>
+    </>
+  )
+}
+
+export default App

e948a6d4dc40f14261f8e5c6fd754f5c2a85824a
Katherine M <katherinesoftwareengineer@gmail.com>
2025-03-07 14:55:59 -0500

    Section B6(Industry Appropriate Security Features) - configured CORS Mapping in backend to allow cross-origin requests from my frontend url only.

--- backend/src/main/java/com/katdev/Pomotivity/Security/SecurityConfig.java
+++ backend/src/main/java/com/katdev/Pomotivity/Security/SecurityConfig.java
@@ -6,12 +6,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -42,15 +45,35 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
+                .cors(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
                 .formLogin(form -> form
-                        .loginProcessingUrl("/login")
-                        .permitAll()
+                        .loginProcessingUrl("/login").permitAll()
+                        .successHandler((request, response, authentication) -> {
+                            System.out.println("Login Success");
+                        })
+                        .failureHandler((request, response, exception) -> {
+                            System.out.println("Login Failure" + exception.getMessage());
+                        })
                 )
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/signup").permitAll()
                         .anyRequest().authenticated()
                 )
                 .build();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:5173")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
     }
 }

ac702f40af6351a13cc67fbcf2ccaa4970f08f6e
Katherine M <katherinesoftwareengineer@gmail.com>
2025-03-09 22:59:40 -0400

    Section B5(Validation Functionality), B6(Industry-Appropriate Security Features), B7(Design Elements that make the application scalable, B8(User Friendly, functional GUI): Added Signup form with inputs for user information. These inputs are required (Validation functionality) Added the connection between my backend and my frontend via a post request to handle user registration successfully. Also added password type on the password inbox to hide the password when the user creates it (Industry appropriate security features & functional GUI). Utilized MUI components to create a consistent simple GUI - (Design Elements for Scalability & User Friendly, functional GUI).

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,8 +1,110 @@
+import { TextField, Container, Paper, Typography, Box, Avatar, Button, Link } from "@mui/material"
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
+import {Link as RouterLink, useNavigate} from "react-router-dom";
+import axios from "axios";
+import { useState } from "react";
+
+
 function Signup() {
+    const navigate = useNavigate();
+
+    const [user, setUser] = useState({
+        username: "",
+        password: "",
+        email: "",
+        tasks: []
+    })
+
+    const handleInput = (e) => {
+        setUser({...user, [e.target.name]: e.target.value})
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        axios.post("http://localhost:8080/signup", user)
+        .then(res => navigate("/login"))
+        .catch()
+    }
+
     return (
-        <>
-            <span>Hello World - Sign up Page</span>
-        </>
+        <Box
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            minHeight="100vh"
+        >
+            <Container maxWidth="sm">
+                <Paper elevation={3} sx={{py:4, px: 2}}>
+                    <Avatar sx={{
+                        mx: "auto",
+                        bgcolor: "secondary.main",
+                    }}>
+                        <AccountCircleOutlinedIcon 
+                            sx={{textAlign:"center"}}
+                        />
+                    </Avatar>
+                    <Typography 
+                        component="h1"
+                        variant="h5" 
+                        sx={{my: 2, textAlign:"center"}}
+                    >
+                        Sign Up 
+                    </Typography>
+                    <Box
+                        component="form"
+                        sx={{mx: 4}}
+                        onSubmit={handleSubmit}
+                    >
+                        <TextField
+                            placeholder="Enter Username"
+                            id="usernameField"
+                            variant="filled"
+                            name = "username"
+                            fullWidth
+                            required
+                            autoFocus
+                            onChange={handleInput}
+                            sx={{mb:2}}
+                        />
+                        <TextField
+                            placeholder="Enter Email"
+                            id="emailField"
+                            name = "email"
+                            variant="filled"
+                            fullWidth
+                            required
+                            onChange={handleInput}
+                            sx={{mb:2}}
+                        />
+                         <TextField
+                            placeholder="Enter Password"
+                            id="passwordField"
+                            name = "password"
+                            variant="filled"
+                            type="password"
+                            fullWidth
+                            required
+                            onChange={handleInput}
+                            sx={{mb:2}}
+                        />
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            size="large"
+                            sx={{mb: 2}}
+                        >
+                            Sign up
+                        </Button>
+
+                        <Typography sx={{display:"inline"}}>Already Have an Account? </Typography>
+
+                        <Link component={RouterLink} to ="/login">Sign In</Link>
+
+                    </Box>
+                </Paper>
+            </Container>  
+        </Box>
     )
 }
 

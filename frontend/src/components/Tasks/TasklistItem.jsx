171a9d4f2cafc8864b38074711876105be44fd84
Katherine M <katherinesoftwareengineer@gmail.com>
2025-03-17 08:32:31 -0400

    Section B3(DB Component with Crud functionality), Section B8(User Friendly and Functional GUI): Created functional delete task button in the ui

--- frontend/src/components/Tasks/TasklistItem.jsx
+++ frontend/src/components/Tasks/TasklistItem.jsx
@@ -4,9 +4,18 @@ import CheckBoxIcon from '@mui/icons-material/CheckBox';
 import SettingsIcon from '@mui/icons-material/Settings';
 import { useState } from "react";
 import { UpdateTask } from "./UpdateTask";
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+import axios from "axios";
 
 export const TaskListItem = ({task, checked, fetchTasks}) => {
     const [isUpdatingTask, setIsUpdatingTask] = useState(false);
+
+    const handleDelete = () => {
+        axios.delete(`http://localhost:8080/api/v1/tasks/${task.id}`, { withCredentials: true })
+        .then(res => fetchTasks())
+        .catch(err => console.log(err))
+    }
+
     return (
         <Box>
             <ListItem>
@@ -18,6 +27,11 @@ export const TaskListItem = ({task, checked, fetchTasks}) => {
                 <ListItemIcon>
                     <IconButton onClick={() => setIsUpdatingTask(true)}>
                         <SettingsIcon/>
+                    </IconButton>
+                </ListItemIcon>
+                <ListItemIcon>
+                    <IconButton onClick={() => {handleDelete()}}>
+                        <DeleteForeverIcon />
                     </IconButton>
                 </ListItemIcon>
             </ListItem>
